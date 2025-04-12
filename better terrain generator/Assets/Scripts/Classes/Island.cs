627ea3ffa75a0c9820cce2366838dffa28c9502e
zardarz <zakreashlaibah@gmail.com>
2025-04-11 20:00:32 -0500

    added the curve thing for the islands

--- better terrain generator/Assets/Scripts/Classes/Island.cs
+++ better terrain generator/Assets/Scripts/Classes/Island.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Island
 {
-    private AnimationCurve graphCurve;
+    private AnimationCurve islandCurve;
 
     private Vector2Int position;
 
@@ -13,13 +13,31 @@ public class Island
     private readonly int islandScale;
 
     public Island(Vector2Int pos, float radious, float islandRadious, int islandScale) {
+        islandCurve = new();
         position = pos;
         continentRadious = radious; 
         this.islandRadious = islandRadious;
         this.islandScale = islandScale;
+
+        makeWave();
+    }
+
+    private void makeWave() {
+        islandCurve.AddKey(0,1);
+        islandCurve.AddKey(1,1);
+
+        int amountOfKeys = Random.Range(1,10);
+        float keyJump = 1/amountOfKeys;
+
+        for(float i = keyJump; i < amountOfKeys * keyJump; i += keyJump) {
+            float keyHeight = Random.Range(90, 100) / 100f;
+            Debug.Log(keyHeight);
+
+            islandCurve.AddKey(i, keyHeight);
+        }
     }
     public float getWave(float x) {
-        return 1;
+        return islandCurve.Evaluate(x);
     }
 
     public Vector2Int getPos() {
