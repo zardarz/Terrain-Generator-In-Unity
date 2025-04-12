56676489468d29a7a3ba9a11459e915e2fdf0af4
zardarz <zakreashlaibah@gmail.com>
2025-04-11 19:07:46 -0500

    ??????

--- better terrain generator/Assets/Scripts/Classes/Continent.cs
+++ better terrain generator/Assets/Scripts/Classes/Continent.cs
@@ -63,32 +63,43 @@ public class Continent : MonoBehaviour
     }
 
     private float getHeight(Vector2Int currentTile) {
+        float[][] distancesAndValues = new float[2][];
+
         float sum = 0;
+
+        for (int i = 0; i < 2; i++)
+        {
+            distancesAndValues[i] = new float[amountOfIslands];
+        }
 
         for(int i = 0; i < amountOfIslands; i++) {
             Island island = islands[i];
 
-            float distance = GetDistance(currentTile, island);
-            float waveValue = GetWaveValue(island, distance);
+
+            float distance = Vector2.Distance(currentTile, island.getPos());
+
+            if(distance == 0) {
+                distance = 1;
+            }
+
+            distancesAndValues[0][i] = distance;
 
 
-            if(distance != 0) {
-                sum += waveValue / distance;
+            float islandWaveValue = island.getWave(distance);
+            float islandRadious = island.getIslandRadious();
+
+            Debug.Log(islandRadious);
+
+            distancesAndValues[1][i] = islandWaveValue * ((islandRadious / radious) + 1);
+
+
+            if(distancesAndValues[0][i] != 0) {
+                sum += distancesAndValues[1][i] / distancesAndValues[0][i];
             }
 
         }
 
-        return sum / amountOfIslands * 10;
-    }
-    
-    private float GetDistance(Vector2Int currentTile, Island island) {
-        float distance = Vector2.Distance(currentTile, island.getPos());
-
-        if(distance == 0) {
-            distance = 1;
-        }
-
-        return distance;
+        return sum / amountOfIslands;
     }
 
     private float GetWaveValue(Island island, float distance) {
